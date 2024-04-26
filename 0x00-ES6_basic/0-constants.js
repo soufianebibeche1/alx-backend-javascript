0cefe3fe65509070b4db1add1f638a21016fd838
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-04-26 22:27:11 +0000

    fix

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let message = 'But sometimes let';
-    message += getLast();
-  
-    return message;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
