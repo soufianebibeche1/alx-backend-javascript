213b55f5f409975fe02a4d638da52457fb75afb0
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-04-28 00:55:26 +0000

    add all the scripts

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,11 @@
-class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+// 7-airport.js
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    toString() {
-        return `[object ${this._code}]`;
-    }
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
-
-export default Airport;
