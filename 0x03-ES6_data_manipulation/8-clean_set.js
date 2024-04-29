65a1d8220261c653647c94e8f654d307c5d6265d
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-04-29 22:18:02 +0000

    add more scripts

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+  let result = '';
+  set.forEach(value => {
+    if (value.startsWith(startString)) {
+      result += value.slice(startString.length) + '-';
+    }
+  });
+  return result.slice(0, -1);
+}
