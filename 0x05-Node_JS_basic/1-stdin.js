df7b03f92789d9a1becda18cbc1e098f495725e2
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-06-19 01:14:17 +0000

    add tassks

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
